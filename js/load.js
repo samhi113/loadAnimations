7b17043b57c609925e2bb187917034fef11a3e6c
Sam Higgins <86005303+samhi113@users.noreply.github.com>
2025-11-25 11:47:21 -0500

    Add files via upload

--- js/load.js
+++ js/load.js
@@ -1,6 +1,26 @@
+const text = document.querySelector("h2");
+const gif = document.getElementById("loading");
+
 window.onload = async function() {
-  delay(2500);
-  document.getElementById("loading").style.opacity = 1;
+  await delay(2500);
+
+  gif.style.opacity = 1;
+
+  var waitTime = (Math.random() * 3000) + 4500;
+  console.log(waitTime);
+  await delay(waitTime);
+
+  text.style.display = "none";
+  gif.style.display = "none";
+
+  waitTime = (Math.random() * 1000) + 2000;
+  console.log(waitTime);
+  await delay(waitTime);
+
+  document.querySelector("body").style.backgroundColor = "#ddd";
+  await delay(2001);
+
+  document.location.href = "page1.html";
 }
 
 function delay(time) {
